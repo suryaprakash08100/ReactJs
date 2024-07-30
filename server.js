f79be4742609daf50f69cb2785e34b387d96d564
suryaprakash08100 <147241454+suryaprakash08100@users.noreply.github.com>
2024-07-30 16:52:23 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
-var http=require('http');
-http.createServer((req,res)=>{
-    res.write(req.url);
-    res.end();
-    console.log("Server is Running")
-}).listen(8080)
+const express=require('express');
+const app=express();
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello World</h1>')
+})
+const products=[
+{
+    id:1,
+    name:"iphone"
+},
+{
+    id:2,
+    name:"Galaxy S2"
+},
+{
+    id:3,
+    name:"Redmi"
+}
+]
+app.get('/products',(req,res)=>{
+    res.json(products)
+})
+app.get('/products/:id',(req,res)=>{
+    const newData=products.filter(items =>items.id.toString()===req.params.id);
+    return res.send(newData)
+})
+app.post('/addproducts',(req,res)=>{
+    const {id,name}=req.body;
+    console.log(id,name)
+    return res.send('Data Stored!')
+}
+)
+app.listen(5000,()=>console.log('Server Running'));
