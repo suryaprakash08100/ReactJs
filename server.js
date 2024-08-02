bb7048de0bdbeae5baadef00d982b74f6fe6c644
suryaprakash08100 <147241454+suryaprakash08100@users.noreply.github.com>
2024-08-02 14:15:04 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,34 +1,16 @@
-const express=require('express');
-const app=express();
-app.use(express.json());
-app.get('/',(req,res)=>{
-    res.send('<h1>Hello World</h1>')
+const express=require('express')
+const dotenv=require('dotenv')
+const {MongoClient}=require('mongodb')
+const app=express()
+dotenv.config()
+MongoClient.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("Mongodb connected successfully")
+}).catch((error)=>{
+    console.log("Error",error)
 })
-const products=[
-{
-    id:1,
-    name:"iphone"
-},
-{
-    id:2,
-    name:"Galaxy S2"
-},
-{
-    id:3,
-    name:"Redmi"
-}
-]
-app.get('/products',(req,res)=>{
-    res.json(products)
+console.log(process.env)
+
+app.listen(3000,()=>{
+    console.log('Server running')
 })
-app.get('/products/:id',(req,res)=>{
-    const newData=products.filter(items =>items.id.toString()===req.params.id);
-    return res.send(newData)
-})
-app.post('/addproducts',(req,res)=>{
-    const {id,name}=req.body;
-    console.log(id,name)
-    return res.send('Data Stored!')
-}
-)
-app.listen(5000,()=>console.log('Server Running'));
